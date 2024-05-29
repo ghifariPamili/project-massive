4520c3efd51febbfe1425b1af72069e5c51de7a7
Frdanta11 <134994374+Frdanta11@users.noreply.github.com>
2024-05-30 01:17:26 +0700

    danta1

--- frontend/src/Components/AboutComponent.jsx
+++ frontend/src/Components/AboutComponent.jsx
@@ -1,7 +1,17 @@
 
 function AboutComponent() {
   return (
-    <div className="h-screen">AboutComponent</div>
+    <div className="bg-white text-black h-screen flex items-center justify-center">
+    <div className="text-center p-6">
+      <h1 className="text-5xl font-bold mb-4">Lebih dari 1000+ mahasiswa mempercayai kami</h1>
+      <p className="text-xl mb-6">JasTip MaMa memungkinkan mahasiswa menitipkan pesanan dan membuka layanan jastip antar mahasiswa di kampus.</p>
+      <div className="flex items-center justify-center">
+        <img src="/src/assets/MH1.jpg" alt="" className="h-55 w-20 mr-4 rounded-lg m-2" />
+        <img src="/src/assets/MH1.jpg" alt="" className="h-55 w-20 mr-4 rounded-lg m-2" />
+        <img src="/src/assets/MH1.jpg" alt="" className="h-55 w-20 rounded-lg m-2" />
+      </div>
+    </div>
+  </div>
   )
 }
 
