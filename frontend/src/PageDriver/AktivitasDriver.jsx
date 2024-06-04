725513df8d2997aadf69a018452f53a3d77d3c6d
Ghifari <ghifaripamili@gmail.com>
2024-06-04 10:14:26 +0800

    commit28

--- frontend/src/PageDriver/AktivitasDriver.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function AktivitasDriver() {
-  return (
-    <div>AktivitasDriver</div>
-  )
-}
-
-export default AktivitasDriver
---
+++ frontend/src/PagePenitip/RiwayatUser.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RiwayatUser() {
+  return (
+    <div>RiwayatUser</div>
+  )
+}
+
+export default RiwayatUser
