74fcf6d05c5f6a36b763316c5507fa9ed5895eab
Ghifari <ghifaripamili@gmail.com>
2024-06-13 08:10:04 +0800

    commit901

--- frontend/src/PagePenitip/BerandaComptUser.jsx
+++ frontend/src/PagePenitip/BerandaComptUser.jsx
@@ -59,21 +59,21 @@ function BerandaComptUser() {
           </div>
           
         </div>
-      ))}
-    </div>
-  );
-}
-
-function FeaturedServices({ services }) {
-  return (
-    <div className="bg-cyan-100 p-6 rounded-lg shadow-md">
-      <h2 className="text-xl font-semibold mb-4">Jastip yang lagi ada ..</h2>
-      <div className="flex justify-between items-center">
-        <div className="w-[300px]">
-          {services.map((service, index) => (
-            <div key={index} className="bg-cyan-300 rounded-[20px] border-black border-[2px]  flex justify-between mb-2">
-              <p>{service.name}</p>
-              <p>{service.price}</p>
+        <div className="BodyList flex flex-col justify-start items-start bg-blue-400 w-full h-[600px] p-[10px]">
+          <div className='title text-[20px]'>
+            Jastip yang lagi ada..
+          </div>
+          <div className='isiList w-full flex flex-row justify-between items-center h-full gap-[10px] p-[5px] bg-black'>
+            <div className="list bg-white h-full p-[5px] w-[60%]">
+            
+              <div className="navigation-buttons p-[1px] flex justify-between items-center h-full w-full">
+                <button onClick={handlePrevious} className="previous-button bg-gray-200 w-[40px] h-[40px] rounded ">Previous</button>
+                {sections[currentSectionIndex].component}
+                <button onClick={handleNext} className="next-button bg-gray-200 w-[40px] h-[40px] rounded">Next</button>
+              </div>
+            </div>
+            <div className="information bg-white w-[40%] h-full flex justify-center items-center p-[40px] text-center">
+              klik list jastip disamping untuk melihat detail jastip
             </div>
           </div>
         </div>
